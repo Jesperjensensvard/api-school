8434929c78797ddea372d2e69d445198c696244b
Jesperjensensvard <43741171+Jesperjensensvard@users.noreply.github.com>
2019-11-15 00:28:10 +0100

    fixses

--- public/space.js
+++ public/space.js
@@ -1,32 +1,39 @@
 const baseUral = 'https://api.nasa.gov/mars-photos/api/v1/'
 const nasaApiKey = 'zdZn3GS1CfaUIeEi7y9lG3mSxqsfVZ7SvljDkZin'
 const rover = 'Curiousity'
-
-async function make_Request() {
-
-}
-
+console.log(true)
 
 async function init_site() {
+    console.log(true)
     try{
-        let photos = await photos()
+        let data = await make_init_url()
+        append_list_of_opitons(data)
     }
     catch{
         console.error()
     }
 }
 
-async function nasa_search() {
-    
+async function make_Request(url) {
+    let respons = await fetch(url)
+    if(respons != 200) {
+        console.log("errorroro")
+        throw new Error(respons.status + ": " + respons.statusText)
+    }else{
+        console.log("success")
+        return await respons.json()
+    }
 }
 
 
-
 function make_init_url() {
-
+    let url = new URL(baseUral + "manifest/" + rover)
+    url.search = new URLSearchParams({
+        api_key: nasaApiKey
+    })
+    return make_Request(url)
 } 
     
-
-function make_nasa_search_url() {
+function append_list_of_opitons(data) {
 
 }
